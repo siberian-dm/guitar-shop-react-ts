bdc344b8f577e44c80ee7344c0e81152cf7847e8
Dmitry Vorobev <dm.siberian@gmail.com>
2021-12-17 23:14:26 +0300

    refactor: split main view into components

---
+++ src/components/common/breadcrumbs/breadcrumbs.tsx
@@ -0,0 +1,18 @@
+function Breadcrumbs(): JSX.Element {
+  return (
+    <ul className="breadcrumbs page-content__breadcrumbs">
+      <li className="breadcrumbs__item">
+        <a className="link" href="./main.html">
+          Главная
+        </a>
+      </li>
+      <li className="breadcrumbs__item">
+        <a className="link" href="#no_scroll">
+          Каталог
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Breadcrumbs;
