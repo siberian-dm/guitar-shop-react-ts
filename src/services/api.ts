11ceffaca2cb2370f775005eb9f547add86f9865
Dmitry Vorobev <dm.siberian@gmail.com>
2021-12-29 04:37:46 +0300

    fix: implement search on the server, add debounce to the search function

--- src/services/api.ts
+++ src/services/api.ts
@@ -5,6 +5,7 @@ const REQUEST_TIMEOUT = 5000;
 
 export enum APIRoute {
   Guitars = '/guitars',
+  GuitarsSearch = '/guitars?name_like=',
   GuitarsWithComments = '/guitars?_embed=comments',
   GuitarWithMinPrice = '/guitars?_order=asc&_sort=price&_start=0&_limit=1',
   GuitarWithMaxPrice = '/guitars?_order=desc&_sort=price&_start=0&_limit=1',
