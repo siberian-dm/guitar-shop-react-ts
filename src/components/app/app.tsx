409947eb9bad77327031b76ada9960f67ebfdf65
Dmitry Vorobev <dm.siberian@gmail.com>
2022-01-09 02:39:32 +0300

    feat: add use-query-params library

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,27 +8,30 @@ import {
   Route,
   Switch
 } from 'react-router-dom';
+import { QueryParamProvider } from 'use-query-params';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path={AppRoute.Root} exact>
-          <Redirect to={AppRoute.CatalogInitialPage}/>
-        </Route>
-        <Route path={AppRoute.Catalog} exact>
-          <Catalog />
-        </Route>
-        <Route path={AppRoute.ProductById} exact>
-          <Product />
-        </Route>
-        <Route path={AppRoute.NotFound} exact>
-          <NotFound />
-        </Route>
-        <Route>
-          <Redirect to={AppRoute.NotFound}/>
-        </Route>
-      </Switch>
+      <QueryParamProvider ReactRouterRoute={Route}>
+        <Switch>
+          <Route path={AppRoute.Root} exact>
+            <Redirect to={AppRoute.CatalogInitialPage}/>
+          </Route>
+          <Route path={AppRoute.Catalog} exact>
+            <Catalog />
+          </Route>
+          <Route path={AppRoute.ProductById} exact>
+            <Product />
+          </Route>
+          <Route path={AppRoute.NotFound} exact>
+            <NotFound />
+          </Route>
+          <Route>
+            <Redirect to={AppRoute.NotFound}/>
+          </Route>
+        </Switch>
+      </QueryParamProvider>
     </BrowserRouter>
   );
 }
