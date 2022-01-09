1e84a844060173ded17a6805d9275bb89fa0542f
Dmitry Vorobev <dm.siberian@gmail.com>
2022-01-09 22:41:59 +0300

    unit-tests: implement testing of parsePageNumberFromString function

--- src/utils/common.test.ts
+++ src/utils/common.test.ts
@@ -1,4 +1,4 @@
-import { formatPrice, parseIntNumberFromString } from './common';
+import { formatPrice, parseIntNumberFromString, parsePageNumberFromString } from './common';
 
 describe('Function: formatPrice', () => {
   it('should return "1 010 009 ₽" when price is 1010009', () => {
@@ -24,5 +24,20 @@ describe('Function: parseIntNumberFromString', () => {
   });
   it('should return null when string is ""', () => {
     expect(parseIntNumberFromString('')).toBe(null);
+  });
+});
+
+describe('Function: parsePageNumberFromString', () => {
+  it('should return 5 when string is "5"', () => {
+    expect(parsePageNumberFromString('5')).toBe(5);
+  });
+  it('should return 5 when string is "5abc"', () => {
+    expect(parsePageNumberFromString('5abc')).toBe(5);
+  });
+  it('should return 1 when string is undefined"', () => {
+    expect(parsePageNumberFromString(undefined)).toBe(1);
+  });
+  it('should return 1 when string is "abc5"', () => {
+    expect(parsePageNumberFromString('abc5')).toBe(1);
   });
 });
