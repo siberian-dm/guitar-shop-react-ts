f6fcce1e01f29faebfd4178773cf91d06180e37f
Dmitry Vorobev <dm.siberian@gmail.com>
2022-01-12 14:00:12 +0300

    fix: fix App and Header components testing

--- src/components/common/header/header.test.tsx
+++ src/components/common/header/header.test.tsx
@@ -1,16 +1,31 @@
 import Header from './header';
+import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
+import { initialState as searchFormInitialState } from '../../../store/reducers/search-form-slice/search-form-slice';
+import { Provider } from 'react-redux';
+import { ReducerName } from '../../../types/store';
 import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 
+const mockStore = configureMockStore();
+
+const store = mockStore({
+  [ReducerName.SearchForm]: {...searchFormInitialState},
+});
+
+const history = createMemoryHistory();
+
+const fakeHeader = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Header />
+    </Router>
+  </Provider>
+);
+
 describe('Component: Header', () => {
   it ('should render correctly', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={history}>
-        <Header />
-      </Router>,
-    );
+    render(fakeHeader);
 
     const links = screen.getAllByRole('link');
 
